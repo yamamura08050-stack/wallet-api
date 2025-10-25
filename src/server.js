db2862d627d16beee55a5edfb306e602f50ce9b0
yamamura08050-stack <yamamura.08050@gmail.com>
2025-10-26 00:24:25 +0900

    cron.js added

--- src/server.js
+++ src/server.js
@@ -3,15 +3,22 @@ import dotenv from "dotenv";
 import { initDB } from "./config/db.js";
 import  rateLimiter  from "./middleware/rateLimiter.js";
 import transactionsRoute from "./routes/transactionsRoute.js";
+import job from "./config/cron.js";
 
 dotenv.config();
 
 const app = express();
 
+if(process.env.NODE_ENV=== "protection") job.start();
+
 app.use(express.json());
 app.use(rateLimiter);
 
 const PORT = process.env.PORT || 5001;
+
+app.get("/api/health", (req, res) => {
+    res.status(200).json({status:"ok"})
+})
 
 app.use("/api/transactions", transactionsRoute)
 
